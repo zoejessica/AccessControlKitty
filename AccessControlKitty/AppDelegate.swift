72ce69b9045db26412090781102a4856878bc7ae
zoejessica <zoews@me.com>
2018-04-28 06:46:06 -0400

    Initial Commit

---
+++ AccessControlKitty/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AccessControlKitty
+//
+//  Created by Zoe Smith on 4/28/18.
+//  Copyright © 2018 Hot Beverage. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
