946ca9de0e2aeddbc756c8e0ef898a2e3d2f1919
zoejessica <zoews@me.com>
2019-02-17 15:54:41 -0500

    Adds Window menu to the application; removes full screen capability from window; app quits on window close

--- AccessControlKitty/AppDelegate.swift
+++ AccessControlKitty/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true 
+    }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
