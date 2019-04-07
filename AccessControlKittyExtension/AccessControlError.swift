6a40692c67b12a096d34d71d4b10859122c7d1b4
Zoë Smith <zoews@me.com>
2019-04-07 07:41:13 -0400

    Add AccessControlError.swift

---
+++ AccessControlKittyExtension/AccessControlError.swift
@@ -0,0 +1,9 @@
+//
+//  AccessControlError.swift
+//  AccessControlKittyExtension
+//
+//  Created by Zoe Smith on 7/4/19.
+//  Copyright © 2019 Hot Beverage. All rights reserved.
+//
+
+import Foundation
